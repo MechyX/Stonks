d7bbb0f972a6847661acbf2677085caa0bcf0d60
mechy <52533782+MechyX@users.noreply.github.com>
2021-11-07 22:02:31 +0530

    intraday plotter complete and added several dep

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,7 +1,14 @@
 import Service.AlphaVantageHelper;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import Service.SymbolPlotter;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.json.JSONObject;
 
 public class app {
+
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.configure()
                 .directory(".")
@@ -9,9 +16,30 @@ public class app {
                 .ignoreIfMissing()
                 .load();
         String apiKey = dotenv.get("ALPHA_VANTAGE_API_KEY");
-        System.out.println(apiKey);
         AlphaVantageHelper helper = new AlphaVantageHelper(apiKey);
-        String res = helper.dailyTimeSeries("IBM");
+        String res = helper.latestTimeSeriesIntraday("IBM", 5);
         System.out.println(res);
+        ArrayList<String> timeSeriesString = null;
+        ArrayList<Double> openValues = new ArrayList<>();
+
+        try {
+            JSONObject jsonObject = new JSONObject(res);
+            JSONObject data = (JSONObject) jsonObject.get("Time Series (5min)");
+            timeSeriesString = (ArrayList<String>) data.keySet().stream().sorted().
+                    collect(Collectors.toList());
+
+            for(int i = 0; i < timeSeriesString.size(); i++){
+                String key = timeSeriesString.get(i);
+                JSONObject profile = (JSONObject) data.get(key);
+                double openValue = Double.parseDouble((String)profile.get("1. open"));
+                openValues.add(openValue);
+            }
+
+            BufferedImage img = SymbolPlotter.plotLatestTimeSeriesIntraday(timeSeriesString, openValues);
+        }  catch (Exception err){
+            err.printStackTrace();
+        }
+
+
     }
 }
